ba12116e546cc9152737fbd34e5a79591c5e033b
Caio Mota <caiocvlmota@gmail.com>
2025-01-27 17:31:59 -0300

    documentaçao do codigo

--- pedidos.js
+++ pedidos.js
@@ -1,5 +1,6 @@
-// Classe que representa um insumo
 /**
+ * Classe que representa um insumo
+ *
  * @class Insumo
  * @constructor
  * @param {number} id - Id do insumo
@@ -9,17 +10,55 @@
  * @param {string} descricao - Descrição do insumo
  */
 class Insumo {
+    /**
+     * @constructor
+     * @param {number} id - Id do insumo
+     * @param {string} nome - Nome do insumo
+     * @param {number} estoque - Quantidade do insumo em estoque
+     * @param {string} imagem - URL da imagem do insumo
+     * @param {string} descricao - Descrição do insumo
+     */
     constructor(id, nome, estoque, imagem, descricao) {
+        /**
+         * Id do insumo
+         *
+         * @type {number}
+         */
         this.id = id;
+
+        /**
+         * Nome do insumo
+         *
+         * @type {string}
+         */
         this.nome = nome;
+
+        /**
+         * Quantidade do insumo em estoque
+         *
+         * @type {number}
+         */
         this.estoque = estoque;
+
+        /**
+         * URL da imagem do insumo
+         *
+         * @type {string}
+         */
         this.imagem = imagem;
+
+        /**
+         * Descrição do insumo
+         *
+         * @type {string}
+         */
         this.descricao = descricao;
     }
 }
 
-// Classe que representa um pedido
 /**
+ * Classe que representa um pedido
+ *
  * @class Pedido
  * @constructor
  * @param {number} id - Id do pedido
@@ -27,9 +66,32 @@ class Insumo {
  * @param {date} data - Data do pedido
  */
 class Pedido {
+    /**
+     * @constructor
+     * @param {number} id - Id do pedido
+     * @param {array} array - Array de insumos do pedido
+     * @param {date} data - Data do pedido
+     */
     constructor(id, array, data) {
+        /**
+         * Id do pedido
+         *
+         * @type {number}
+         */
         this.id = id;
+
+        /**
+         * Array de insumos do pedido
+         *
+         * @type {array}
+         */
         this.array = array;
+
+        /**
+         * Data do pedido
+         *
+         * @type {date}
+         */
         this.data = data;
     }
 }
@@ -58,14 +120,19 @@ let insumos = [
     { id: 20, nome: "curativo", imagem: 'https://picsum.photos/200/300', estoque: 400, descricao: "Curativo adesivo para pequenos cortes e arranhões." }
 ];
 
-// Lista de pedidos realizados
+/**
+ * Lista de pedidos realizados
+ * @type {array}
+ */
 let pedidos = [];
 
 /**
  * Preenche a lista de insumos no HTML com botões para adicionar ao pedido
  */
 function preencherListaDeInsumos() {
+    // Preenche a lista de insumos
     document.getElementById('lista').innerHTML = getListaDeInsumos().map((item) => {
+        // Exibe a lista de insumos
         document.getElementById('lista').style.display = 'grid';
         document.getElementById('lista').style.gridTemplateColumns = 'repeat(auto-fill, minmax(200px, 1fr))';
         return `<div class="item">
@@ -78,6 +145,7 @@ function preencherListaDeInsumos() {
         </div>`;
     }).join('');
 }
+// Chama a função para preencher a lista de insumos
 preencherListaDeInsumos();
 
 // Elemento de entrada para pesquisa
@@ -86,6 +154,7 @@ const inputPesquisa = document.getElementById('pesquisa');
 // Botão para pesquisar
 let botaopesquisar = document.getElementById('botaopesquisar');
 botaopesquisar.addEventListener('click', () => {
+    // Exibe a lista de insumos
     document.getElementById('lista').style.display = 'block';
 });
 
@@ -93,9 +162,12 @@ botaopesquisar.addEventListener('click', () => {
  * Evento para filtrar itens com base na pesquisa
  */
 inputPesquisa.addEventListener('input', () => {
+    // Valor da pesquisa
     let valor = formatarString(inputPesquisa.value);
+    // Elementos da lista de insumos
     const items = document.querySelectorAll('.item');
 
+    // Percorre a lista de insumos e esconde os que não contém o valor da pesquisa
     items.forEach((element) => {
         if (formatarString(element.textContent).indexOf(valor) === -1) {
             element.style.display = 'none';
@@ -114,51 +186,67 @@ let arrayItensSelecionados = [];
  */
 document.getElementById('lista').addEventListener('click', (event) => {
     if (event.target && event.target.matches('button.addBtn')) {
+        // Item selecionado
         let item = event.target.parentElement.querySelector('li.nomeItem').textContent;
+        // Quantidade do item
         let quantidade = parseInt(event.target.parentElement.querySelector('input.quantidade').value, 10);
         if (isNaN(quantidade)) {
             quantidade = 1;
         }
+        // Verifica se o item já existe na lista de itens selecionados
         let itemExistente = arrayItensSelecionados.find(i => i.nome === item);
         if (itemExistente) {
+            // Incrementa a quantidade do item existente
             itemExistente.quantidade += quantidade;
         } else {
+            // Adiciona o item à lista de itens selecionados
             arrayItensSelecionados.push({ nome: item, quantidade });
         }
+        // Atualiza a lista de itens selecionados
         atualizarListaSelecionados();
     }
 });
 
+/**
+ * Atualiza a lista de itens selecionados
+ */
 function atualizarListaSelecionados() {
-    let lista = document.getElementById('lista');
+    // Elemento da lista de itens selecionados
     let listaSelecionados = document.getElementById('listaSelecionados');
+    // Gera a lista de itens selecionados
     listaSelecionados.innerHTML = arrayItensSelecionados.map(item => 
         `<div class="itemSelecionado" style="display: flex; align-items: center;">
             <li>${item.nome} (Quantidade: ${item.quantidade})</li>
             <button class="deleteBtn" style="margin-left: auto;">Delete</button>
         </div>`
     ).join('');
+    // Exibe a lista de itens selecionados
     listaSelecionados.style.display = 'block';
 }
-
 
 /**
  * Evento para remover item selecionado
  */
 document.getElementById('listaSelecionados').addEventListener('click', (event) => {
     if (event.target && event.target.matches('button.deleteBtn')) {
+        // Item a ser removido
         let itemDiv = event.target.parentElement;
         let itemName = itemDiv.querySelector('li').textContent.split(' (')[0];
+        // Remove o item da lista de itens selecionados
         arrayItensSelecionados = arrayItensSelecionados.filter(item => item.nome !== itemName);
+        // Remove o item da lista de itens selecionados
         itemDiv.remove();
     }
 });
 
 // Evento para confirmar o pedido
 botaoConfirmar.addEventListener('click', () => {
+    // Cria um novo pedido
     let pedido = new Pedido(pedidos.length + 1, arrayItensSelecionados, new Date());
+    // Adiciona o pedido à lista de pedidos
     pedidos.push(pedido);
 
+    // Atualiza o estoque dos insumos
     arrayItensSelecionados.forEach(item => {
         let insumo = insumos.find(insumo => insumo.nome === item.nome);
         if (insumo && insumo.estoque >= item.quantidade) {
@@ -166,16 +254,21 @@ botaoConfirmar.addEventListener('click', () => {
         }
     });
 
+    // Limpa a lista de itens selecionados
     arrayItensSelecionados = [];
+    // Limpa a lista de itens selecionados
     let listaSelecionados = document.getElementById('listaSelecionados');
     listaSelecionados.innerHTML = '';
     listaSelecionados.style.display = 'none';
+    // Atualiza a lista de insumos
     preencherListaDeInsumos();
+    // Mostra uma mensagem de confirmação
     alert('Pedido Realizado!');
-    console.log(pedidos);
+    // Mostra o histórico de pedidos
     let historicoDePedidos = document.getElementById('historico');
     historicoDePedidos.innerHTML += `<p>Pedido ${pedido.id} - ${pedido.data.toLocaleString('pt-BR', { day: 'numeric', month: 'long', year: 'numeric', hour: '2-digit', minute: '2-digit' })} - Setor: ${setor.value} - Tipo: ${tipo.value} - ${pedido.array.map(i => i.nome + ' (' + i.quantidade + ')').join(', ')}</p>`;
 });
+
 
 /**
  * Formata a string para comparação
@@ -210,7 +303,4 @@ function criarInsumo(nome, estoque) {
 function deletarInsumo(id) {
     insumos.splice(id - 1, 1);
 }
-
-
-
 
